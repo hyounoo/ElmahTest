15ca8e294ec72889568efc49c60f74a30484be9f
Hyounwoo <hyounoosung@gmail.com>
2017-07-04 18:01:23 +0900

    ElmahTest

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElmahTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            int a = 0;
+            int b;
+            //b = 1 / a;
+            HandleExceptions();
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        static string[] GetAllFiles(string str)
+        {
+            // Should throw an AccessDenied exception on Vista. 
+            return System.IO.Directory.GetFiles(str, "*.txt", System.IO.SearchOption.AllDirectories);
+        }
+        static void HandleExceptions()
+        {
+            // Assume this is a user-entered string. 
+            string path = @"C:\";
+
+            // Use this line to throw UnauthorizedAccessException, which we handle.
+            Task<string[]> task1 = Task<string[]>.Factory.StartNew(() => GetAllFiles(path));
+
+            // Use this line to throw an exception that is not handled. 
+            //  Task task1 = Task.Factory.StartNew(() => { throw new IndexOutOfRangeException(); } ); 
+            try
+            {
+                task1.Wait();
+            }
+            catch (AggregateException ae)
+            {
+
+                throw ae;
+
+            }
+
+            Console.WriteLine("task1 has completed.");
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ElmahTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ElmahTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ad1b9ee-d161-48d7-bb64-92e7da81e659")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
